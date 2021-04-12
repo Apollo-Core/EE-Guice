71c4bae1fd183c8c2d4d4cdd8c3bdfea36fed972
Fedor Smirnov <fedor@dps.uibk.ac.at>
2021-04-12 13:58:05 +0200

    following the core changes

--- src/main/java/at/uibk/dps/ee/guice/EeCoreInjectable.java
+++ src/main/java/at/uibk/dps/ee/guice/EeCoreInjectable.java
@@ -6,7 +6,6 @@ import com.google.inject.Inject;
 
 import at.uibk.dps.ee.core.EeCore;
 import at.uibk.dps.ee.core.EnactableProvider;
-import at.uibk.dps.ee.core.InputDataProvider;
 import at.uibk.dps.ee.core.OutputDataHandler;
 import at.uibk.dps.ee.core.enactable.EnactmentStateListener;
 
@@ -23,7 +22,6 @@ public class EeCoreInjectable extends EeCore {
    * This is the constructor which will be used during the dynamic dependency
    * injection.
    * 
-   * @param inputDataProvider the component providing the input processing data
    * @param outputDataHandler the component handling the data produced when
    *        running the workflow
    * @param enactableProvider the component providing the root enactable which is
@@ -32,9 +30,8 @@ public class EeCoreInjectable extends EeCore {
    *        different states of the enactment
    */
   @Inject
-  public EeCoreInjectable(final InputDataProvider inputDataProvider,
-      final OutputDataHandler outputDataHandler, final EnactableProvider enactableProvider,
-      final Set<EnactmentStateListener> stateListeners) {
-    super(inputDataProvider, outputDataHandler, enactableProvider, stateListeners);
+  public EeCoreInjectable(final OutputDataHandler outputDataHandler,
+      final EnactableProvider enactableProvider, final Set<EnactmentStateListener> stateListeners) {
+    super(outputDataHandler, enactableProvider, stateListeners);
   }
 }
--- src/main/java/at/uibk/dps/ee/guice/starter/EeTask.java
+++ src/main/java/at/uibk/dps/ee/guice/starter/EeTask.java
@@ -1,26 +1,34 @@
 package at.uibk.dps.ee.guice.starter;
 
 import org.opt4j.core.start.Opt4JTask;
-
+import com.google.gson.JsonObject;
+import at.uibk.dps.ee.core.InputDataProvider;
 import at.uibk.dps.ee.core.exception.FailureException;
 import at.uibk.dps.ee.guice.EeCoreInjectable;
 
 /**
  * The {@link EeTask} triggers the enactment of a workflow based on a
- * module-driven configuration of the enactment process.
+ * module-driven configuration of the enactment process. Used for enactments which are activated via the configuration GUI.
  * 
  * @author Fedor Smirnov
  *
  */
 public class EeTask extends Opt4JTask {
 
-  protected EeCoreInjectable eeCore;
-
   @Override
   public void execute() throws FailureException {
     open();
-    eeCore = injector.getInstance(EeCoreInjectable.class);
-    eeCore.enactWorkflow();
+    EeCoreInjectable eeCore = injector.getInstance(EeCoreInjectable.class);
+    InputDataProvider inputProvider =  new InputDataProvider() {
+      
+      @Override
+      public JsonObject getInputData() {
+        
+        return new JsonObject();
+      }
+    }; //injector.getInstance(InputDataProvider.class);
+    JsonObject inputData = inputProvider.getInputData();
+    eeCore.enactWorkflow(inputData);
     if (closeOnStop) {
       close();
     }
